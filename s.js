e0c5c1fcb4b191034e5103e15b9e40c1ea9918b4
vunam <nam.vh85@gmail.com>
2020-09-24 16:54:40 +0700

    u

--- s.js
+++ s.js
@@ -1,49 +1,44 @@
 // inputs = document.forms['register'].getElementsByTagName('input');
 inputs = document.getElementsByTagName('input')
 run_onchange = false;
-
 valid = () => {
-        let errors = false;
-        let reg_mail = /^[A-Za-z0-9]+([_\.\-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\.\-]?[A-Za-z0-9]+)*(\.[A-Za-z]+)+$/;
-        for (const i in inputs)
-            if (inputs.hasOwnProperty(i)) {
-                let value = inputs[i].value;
-                let id = inputs[i].getAttribute('id');
-                let span = document.createElement('span');
-                let p = inputs[i].parentNode;
-                // if (p.lastChild.nodeName == 'SPAN') p.removeChild(p.lastChild);
-                if (p.lastChild.nodeName.toLowerCase() == 'span') p.removeChild(p.lastChild)
-                if (!value) span.textContent = ' Thông tin được yêu cầu';
-                else {
-                    if (id == 'email' && !reg_mail.test(value))
-                        span.innerHTML = ' Email không hợp lệ (ví dụ: abc@gmail.com)';
-                    if (id == 'password') {
-                        if (value.length < 6) span.innerHTML = ' Password phải từ 6 ký tự';
-                        var pass = value;
-                    }
-                    if (id == 'confirm_pass' && value != pass) span.innerHTML = ' Password nhập lại chưa đúng';
-                    if (id == 'phone' && isNaN(value)) span.innerHTML = ' Số điện thoại phải là kiểu số';
+    let errors = false;
+    let reg_mail = /^[A-Za-z0-9]+([_\.\-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\.\-]?[A-Za-z0-9]+)*(\.[A-Za-z]+)+$/;
+    for (const i in inputs)
+        if (inputs.hasOwnProperty(i)) {
+            let value = inputs[i].value;
+            let id = inputs[i].getAttribute('id');
+            let span = document.createElement('span');
+            let p = inputs[i].parentNode;
+            // if (p.lastChild.nodeName == 'SPAN') p.removeChild(p.lastChild);
+            if (p.lastChild.nodeName.toLowerCase() == 'span') p.removeChild(p.lastChild)
+            if (!value) span.textContent = ' Thông tin được yêu cầu';
+            else {
+                if (id == 'email' && !reg_mail.test(value))
+                    span.innerHTML = ' Email không hợp lệ (ví dụ: abc@gmail.com)';
+                if (id == 'password') {
+                    if (value.length < 6) span.innerHTML = ' Password phải từ 6 ký tự';
+                    var pass = value;
                 }
-                // Nếu có lỗi thì chèn span vào hồ sơ, chạy onchange, submit return false, highlight border
-                if (span.innerHTML != '') {
-                    inputs[i].parentNode.appendChild(span);
-                    errors = true;
-                    run_onchange = true;
-                    // inputs[i].style.border = '1px solid #c6807b';
-                    // inputs[i].style.background = '#fffcf9';
-                    inputs[i].style.border = '1px solid red';
-                    inputs[i].style.background = 'lightgreen';
-                }
+                if (id == 'confirm_pass' && value != pass) span.innerHTML = ' Password nhập lại chưa đúng';
+                // if (id == 'phone' && isNaN(value)) span.innerHTML = ' Số điện thoại phải là kiểu số';
             }
-        if (!errors) alert('Đăng ký thành công');
-        return !errors;
-    } // end valid()
-    // Chạy hàm kiểm tra valid()
+            if (span.innerHTML != '') {
+                inputs[i].parentNode.appendChild(span);
+                errors = true;
+                run_onchange = true;
+                // inputs[i].style.border = '1px solid #c6807b';
+                // inputs[i].style.background = '#fffcf9';
+                inputs[i].style.border = '1px solid red';
+                inputs[i].style.background = 'lightgreen';
+            }
+        }
+    if (!errors) alert('Đăng ký thành công');
+    return !errors;
+}
 document.getElementById('submit').onclick = () => {
     return valid();
 }
-
-// Kiểm tra lỗi với sự kiện onchange -> gọi lại hàm valid()
 for (const i in inputs)
     if (inputs.hasOwnProperty(i))
         inputs[i].onchange = function() {
